12bdcb351710049a2aaea1568c17c5c0978f2a90
giabach99 <giabach060399@gmail.com>
2023-03-29 19:30:12 +0700

    fixing register api

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const registerHandler = (req, res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => json.status(400).json('Unable to register'))      
+    .catch(err => res.status(400).json('Unable to register'))      
 }
 
 module.exports = {
