d63eb112b19bdb1a4b4fe6c81c8f167bab8de034
giabach99 <giabach060399@gmail.com>
2023-03-30 04:10:11 +0700

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -8,43 +8,28 @@ const registerHandler = (req, res, db, bcrypt) => {
     console.log("logging register email", email);
     console.log("register db", db);
     res.status(200).json('register OK');
-    // db.insert({
-    //             hash: hash,
-    //             email: email
-    //         })
-    //         .into('login')
-    //         .returning('email')
-    //         .then(loginEmail => console.log(loginEmail).catch(err => res.status(400).json('This is error from my test loginEmail', err)));
 
-    // db.transaction(trx => {
-    //     console.log("logging register trx: ", trx);
-    //     trx.insert({
-    //         hash: hash,
-    //         email: email
-    //     })
-    //     .into('login')
-    //     .returning('email')
-    //     .then(loginEmail => {
-    //         console.log("logging login table insertion response", loginEmail);
-    //         console.log('I am adding to login table');
-    //         return trx('users')
-    //             .returning('*')
-    //             .insert({
-    //                 email: loginEmail[0].email,
-    //                 name: name,
-    //                 joined: new Date()
-    //             })
-    //             .then(user => {
-    //                 console.log("logging signedin user extracted", user);
-    //                 return res.status(200).json(user[0]);
-    //             })
-    //     })
-    //     .then(trx.commit)
-    //     .catch(trx.rollback)
-    // }).catch(err => {
-    //     const status = err.status || 500;
-    //     return res.status(status).json('Unable to register, err: ', err);
-    // });
+
+    db.transaction(trx => {
+        console.log("logging register trx: ", trx);
+        trx.insert({
+            hash: hash,
+            email: email
+        }).into('login').returning('email').then(loginEmail => {
+            console.log("logging login table insertion response", loginEmail);
+            console.log('I am adding to login table');
+            return trx('users').returning('*').insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                }).then(user => {
+                    console.log("logging signedin user extracted", user);
+                    return res.status(200).json(user[0]);
+                })
+        }).then(trx.commit).catch(trx.rollback)}).catch(err => {
+                const status = err.status || 500;
+                return res.status(status).json('Unable to register, err: ', err);
+            });
 }
 
 module.exports = {
