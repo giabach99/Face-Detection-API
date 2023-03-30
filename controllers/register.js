725f2e05028d840ad956f9caa55affcd415e419c
giabach99 <giabach060399@gmail.com>
2023-03-30 17:49:28 +0700

    log request

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,5 @@
 const registerHandler = (req, res, db, bcrypt) => {
+    console.log(req);
     const {email, name, password} = req.body;
     if (!email || !name || !password) {
         return res.status(400).json('Incorrect form submission');
